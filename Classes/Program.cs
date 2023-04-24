c76b6aa025cfb8a32d051d0615c75f84558d43f0
Zachari Price <ZachariPrice3@gmail.com>
2023-04-24 03:09:36 -0400

    Classes Exercise1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Tesla";   
+            myCar.Model = "CyberTruck";
+            myCar.Year = 2023;
+
+            var Honda = new Car()
+            {
+                Make = "Honda",
+                Model = "Sonata",
+                Year = 2023
+            };
+
+            var chevy = new Car("Chevy", "Cruz", 2023);
+           
+            var carList = new List<Car>() { myCar, Honda, chevy };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
+
+
         }
     }
 }
+ 
